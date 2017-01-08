005f8407a8d278a92e4e4fd49d5f52c7e21e01f3
W Berard <wberard@math.arizona.edu>
2017-01-07 23:10:08 -0700

    Initial commit of python file to practice nltk stuff.

---
+++ eo_nltk.py
@@ -0,0 +1,12 @@
+
+import nltk
+from nltk.corpus import udhr
+
+# Put the raw text of the Universal Declaration of Human Rights
+# in Esperanto into raw_text.
+raw_text = udhr.raw(u'Esperanto-UTF8')
+
+# Show plot of letter frequency.
+#nltk.FreqDist(raw_text).plot()
+
+
