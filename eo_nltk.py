b4dfcabdba3ca7cec4eac97f8e95108c6bd54ec9
W Berard <wberard@math.arizona.edu>
2017-01-08 22:17:45 -0700

    Add some use of BeautifulSoup to the nltk python file.

--- eo_nltk.py
+++ eo_nltk.py
@@ -1,5 +1,6 @@
 
-import nltk
+import nltk, re, pprint
+from nltk import word_tokenize
 from nltk.corpus import udhr
 
 # Put the raw text of the Universal Declaration of Human Rights
@@ -9,4 +10,11 @@ raw_text = udhr.raw(u'Esperanto-UTF8')
 # Show plot of letter frequency.
 #nltk.FreqDist(raw_text).plot()
 
+from bs4 import BeautifulSoup
 
+raw = BeautifulSoup(raw_text, "html.parser").get_text()
+
+print type(raw)
+tokens = word_tokenize(raw)
+
+print "Num tokens = ", len(tokens)
