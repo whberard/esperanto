7a5b58329eac1a0d79f7e918e822917757f41541
W Berard <wberard@math.arizona.edu>
2017-01-09 22:16:15 -0700

    Read in Gerda Malaperis and create an nltk Text.

--- eo_nltk.py
+++ eo_nltk.py
@@ -5,16 +5,28 @@ from nltk.corpus import udhr
 
 # Put the raw text of the Universal Declaration of Human Rights
 # in Esperanto into raw_text.
-raw_text = udhr.raw(u'Esperanto-UTF8')
+# raw_text = udhr.raw(u'Esperanto-UTF8')
 
 # Show plot of letter frequency.
 #nltk.FreqDist(raw_text).plot()
 
 from bs4 import BeautifulSoup
 
+myfile = open("./tekstaro-de-esperanto/tekstoj-xml/gerda-malaperis.xml")
+raw_text = myfile.read()
+#print raw_text[0:200]
 raw = BeautifulSoup(raw_text, "html.parser").get_text()
+
 
 print type(raw)
 tokens = word_tokenize(raw)
 
 print "Num tokens = ", len(tokens)
+print "First few tokens: ", tokens[0:100]
+
+# print raw.concordance(u'Tiu')
+
+text = nltk.Text(tokens)
+print type(text)
+
+text.collocations()
